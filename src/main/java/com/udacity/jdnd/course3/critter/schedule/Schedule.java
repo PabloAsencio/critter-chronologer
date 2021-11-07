5dacbdf8ee85637b097fe510ce2754f48b404961
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2021-11-07 10:41:04 +0100

    feat: Create Schedule entity

---
+++ src/main/java/com/udacity/jdnd/course3/critter/schedule/Schedule.java
@@ -0,0 +1,84 @@
+package com.udacity.jdnd.course3.critter.schedule;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.user.Employee;
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Schedule {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Employee> employees;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Pet> pets;
+
+    private LocalDate date;
+
+    // See https://stackoverflow.com/questions/416208/jpa-map-collection-of-enums
+    @ElementCollection(targetClass = EmployeeSkill.class)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "schedule_activity")
+    @Column(name = "activity")
+    private List<EmployeeSkill> activities;
+
+    public Schedule() {
+
+    }
+
+    public Schedule(Long id, List<Employee> employees, List<Pet> pets, LocalDate date, List<EmployeeSkill> activities) {
+        this.id = id;
+        this.employees = employees;
+        this.pets = pets;
+        this.date = date;
+        this.activities = activities;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public List<EmployeeSkill> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(List<EmployeeSkill> activities) {
+        this.activities = activities;
+    }
+}
